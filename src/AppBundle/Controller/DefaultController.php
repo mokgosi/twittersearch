174237ef2722355a208c063254a6c271b5eb2e97
mokgosi <mokgosi@gmail.com>
2016-02-15 08:36:37 +0200

    add orderby to sql

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,44 +15,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $twitter = $this->get('endroid.twitter');
-
         $em = $this->getDoctrine()->getManager();
         
-        $tweetStatuses = $em->getRepository('AppBundle:Tweet')->findAll();
+        $tweetStatuses = $em->getRepository('AppBundle:Tweet')->findBy(array(),array('id'=>'desc'));
         
         $form = $this->createFormBuilder()
                 ->add('hashtag', 'text')
                 ->add('search', 'submit')
                 ->getForm();
-
-        $form->handleRequest($request);
-
-//        if ($form->isValid()) {
-//            $data = $form->getData();
-//
-//            $response = $twitter->query("search/tweets", 'GET', 'json', 
-//                    array('q' => $data['hashtag']));
-//
-//            $tweets = json_decode($response->getContent());
-//
-//            foreach ($tweets->statuses as $key => $tweet) {
-//
-//                $entity = new Tweet();
-//                $entity->setText($tweet->text);
-//                $entity->setCreatedAt(new \DateTime($tweet->created_at));
-//                $entity->setUserName($tweet->user->name);
-//                $entity->setScreenName($tweet->user->name);
-//                $entity->setProfileImg($tweet->user->profile_image_url);
-//                $entity->setRetweetCount($tweet->retweet_count);
-//                $entity->setFavouriteCount($tweet->favorite_count);
-//                $entity->setGeo($tweet->geo);
-//                $em->persist($entity);
-//            }
-//            $em->flush();
-//            $em->clear();
-//            $tweetStatuses = $tweets->statuses;
-//        }
         
         return $this->render('default/index.html.twig', array(
                     'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..'),
